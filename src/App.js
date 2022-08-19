5abfacc574363f810e3262eb2573fc74cf8e810d
jayden <jayden@Jayden-Macbook-Pro.local>
2022-08-19 17:29:04 +0900

    Start project

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Orders from "./components/Orders";
+import Prototypes from "./components/Prototypes";
+import AppStateProvider from "./providers/AppStateProvider";
+
+function App() {
+  return (
+    <AppStateProvider>
+      <Header />
+      <div className="container">
+        <Prototypes />
+        <Orders />
+        <Footer />
+      </div>
+    </AppStateProvider>
+  );
+}
+
+export default App;
