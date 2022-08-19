abe5a66b26abd1f2f145d070d1613de6777538be
jayden <jayden@Jayden-Macbook-Pro.local>
2022-08-19 17:38:59 +0900

    저작권 수정

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer>
-      <p>© 2021 Jayden Choi. All rights reserved.</p>
+      <p>© 2021 Mark Lee. All rights reserved.</p>
     </footer>
   );
 }
